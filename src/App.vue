7b75a7057634f22da84e01003901a056cdea190b
Caneco <saulo.m.ozorio@gmail.com>
2022-10-16 00:10:39 -0300

    Reaction Timer - Early

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to Reaction Timer!</h1>
+  <p>Click on the screen when it becomes green.</p>
+  <button :class="{playBtn: !isPlaying}" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/> 
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
 
 export default {
   name: 'App',
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  methods: {
+    start(){
+      this.delay = 1500 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -22,5 +47,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.playBtn{
+  padding: 20px 50px;
+  background: #0faf87;
+  color: white;
+  border-radius: 5px;
+  border: none;
+  font-size: 1.25em;
+  font-weight: bold;
+  cursor: pointer;
+}
+.playBtn:hover{
+  background: #0b7e61;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="reaction">
+    <p>Reaction Time: {{this.score}}ms</p>
+</div>
+<p>Rank: {{this.rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ["score"],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score >= 180 && this.score < 250){
+            this.rank = "Gamer"
+        } else if (this.score >= 250 && this.score < 450){
+            this.rank = "Casual"
+        } else if (this.score >= 450){
+            this.rank = "Old Man"
+        }else{
+            this.rank = "Pro Player"
+        }
+    }
+}
+</script>
+
+<style>
+    .reaction{
+        width: 300px;
+        padding: 2px 0px;
+        margin: 10px auto;
+        text-align: center;
+        color: white;
+        border: 2px solid black;
+        border-radius: 10px;
+        background: rgba(0,0,0,.45);
+    }
+</style>
